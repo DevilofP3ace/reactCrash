3b09cfbcac6f80f3478a1593d4f911c9c8726d23
DevilofP3ace <ritesh@DESKTOP-985O3G8>
2023-05-26 11:22:55 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import ListGroups from "./Components/ListGroups";
+import Buttons from "./Message";
+function App() {
+  let items = ["london", "paris", "newyork"];
+  let ButtonPriority = "button number";
+  const ClickHandler = (Displayer: string) => {
+    console.log(Displayer);
+  };
 
-function App(){
-  
-return <div><ListGroups /></div>
+  const handleEvent = (item: string) => {
+    console.log(item + 22);
+  };
+  const AlertRemover = (alerthand: bool) => {
+    alerthand = false;
+  };
+
+  return (
+    <div>
+      <ListGroups items={items} heading="Cities" onSelectItem={handleEvent} />
+
+      <Buttons
+        ButtonText={ButtonPriority}
+        Clicked={ClickHandler}
+        AlertRemover={}
+      />
+    </div>
+  );
 }
 
 export default App;
